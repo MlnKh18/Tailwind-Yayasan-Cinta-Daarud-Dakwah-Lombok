7aa9e952a17fb71c3f31f67d9118f3b9328e322e
MlnKh18 <116242515+MlnKh18@users.noreply.github.com>
2024-09-21 19:46:30 +0800

    add: proggram section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,18 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        custom: 'Coolvetica Crammed Regular',
+        poppins: 'Poppins'
+      },
       colors: {
         'primary': {
           "default": '#ffe8ba',
           "hover": "#ffffff"
         },
         'secondary': '#0E6678',
+        'third': '#FFA500',
+        'hover-color': 'rgba(67, 67, 67, 0.42)'
       }
     },
   },
